f7e0d2aa515c960884a690a16782bc8a386f6b00
Simon Leary <simon.leary42@proton.me>
2025-02-16 10:53:28 -0500

    refactor

--- discord-message.py
+++ discord-message.py
@@ -15,15 +15,13 @@ message = sys.stdin.read()
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}", file=sys.stderr)
-    channel = client.get_channel(CHANNEL_ID)
-    if channel:
-        try:
-            await channel.send(message)
-        except discord.errors.HTTPException:
-            traceback.print_exc()
-            sys.exit(1)
-    else:
+    if not (channel := client.get_channel(CHANNEL_ID)):
         raise RuntimeError("Channel not found. Check the ID.")
+    try:
+        await channel.send(message)
+    except discord.errors.HTTPException:
+        traceback.print_exc()
+        sys.exit(1)
     await client.close()
 
 
